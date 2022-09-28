ccb6a1ffc9fc8cdb288432e4a3afd06f5aac39ea
xbayazid <bayazid9006@gmail.com>
2022-09-29 01:36:05 +0600

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css'
+
+const Product = ({product, handleAddToCart}) => {
+    // const {product, handleAddToCart} = props;
+    const {name, img, seller, price, ratings} = product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <p className='product-name'>{name}</p>
+            <p>Price: ${price}</p>
+            <p><small>Seller: {seller}</small></p>
+            <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p>Add to cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
